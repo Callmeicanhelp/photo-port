f956d5483eeb03eaa918134b9c3fcf2c618c2c3a
Callmeicanhelp <callmeicanhelp@gmail.com>
2022-08-10 23:37:27 -0400

    tests

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+afterEach(cleanup);
+describe('About component', () => {
+  const { asFragment } = render(<About />);
+  // First test
+  it('renders', () => {
+    render(<About />);
+  });
+
+  // Second test
+  it('matches snapshot DOM node structure', () => {
+    // render About
+  });
+  
+  expect(asFragment()).toMatchSnapshot();
+})
