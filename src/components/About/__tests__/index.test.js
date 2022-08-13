9d3256fdee38a4124f1b3359f21b25b6d44301be
Callmeicanhelp <callmeicanhelp@gmail.com>
2022-08-13 15:31:27 -0400

    lesson snapshot

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,21 +1,19 @@
-import React from "react";
-import { render, cleanup } from "@testing-library/react";
+// __tests__/About.test.js
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
 afterEach(cleanup);
 
-describe('About component', () => {
-  const { asFragment } = render(<About />);
-  // First test
+describe('About component renders', () => {
   it('renders', () => {
     render(<About />);
   });
-
-  // Second test
-  it('matches snapshot DOM node structure', () => {
-    // render About
-  });
   
-  // expect(asFragment()).toMatchSnapshot();
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,29 +1,15 @@
-import React from "react";
-import { render, cleanup } from '@testing-library/react';
+import React from 'react';
+import PhotoList from '../PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import PhotoList from "../PhotoList";
 
-const portrait = { name: "portraits", description: "Portraits of people in my life" };
-
-afterEach(cleanup)
-
-describe('Gallery is rendering', () => {
-
-  it('renders', () => {
-    render(<Gallery currentCategory={portrait} />);
-  });
-})
-
-function Gallery({ currentCategory }) {
-  const { name, description } = currentCategory;
+function Gallery(props) {
+  const { currentCategory } = props;
   return (
     <section>
-      <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
-      <p>{description}</p>
+      <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p>{currentCategory.description}</p>
       <PhotoList category={currentCategory.name} />
     </section>
   );
 }
-
-
 export default Gallery;
