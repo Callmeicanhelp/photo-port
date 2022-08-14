09cb1ea390c7bcbad52a0ea1a5fac9a12ae5de32
Callmeicanhelp <callmeicanhelp@gmail.com>
2022-08-14 18:17:27 -0400

    updated tests

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -36,7 +36,7 @@ function ContactForm() {
   return (
     
     <section>
-      <h1>Contact me</h1>
+      <h1 data-testid="contact" >Contact me</h1>
       <form id="contact-form">
         <div>
           <label htmlFor="name">Name:</label>
@@ -54,7 +54,7 @@ function ContactForm() {
               <p className="error-text">{errorMessage}</p>
             </div>
           )}
-          <button type='submit'>Submit</button>
+          <button data-testid="button" type='submit'>Submit</button>
         </div>
       </form>
     </section>
